c9c23ddf3318201c5b22a25f080be241c0e8aa20
Hiroki Miyabayashi <Hiroki.miya00@gmail.com>
2021-11-12 15:49:10 -0500

    added items database

---
+++ api/src/models/items.model.js
@@ -0,0 +1,71 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const { toDefaultValue } = require('sequelize/types/lib/utils');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const items = sequelizeClient.define('items', {
+    quality: {
+      type: DataTypes.ENUM(
+        'bad',
+        'okay',
+        'good',
+        'excellent'
+      ),
+      allowNull: false,
+      defaultValue: 'good'
+    },
+    where_bought: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    storage: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    must_use_immediately: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    notification_frequency: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    date_bought: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    sell_by: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    percent_wasted: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  items.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+    items.belongsTo(models.foods);
+  };
+
+  return items;
+};
--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,5 +1,7 @@
 const foods = require('./foods/foods.service.js');
+const items = require('./items/items.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(foods);
+  app.configure(items);
 };
