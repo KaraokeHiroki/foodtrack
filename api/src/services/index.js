43ad5dc3c9e2c62fbdb477ac39a11350ff8c25e1
Hiroki Miyabayashi <Hiroki.miya00@gmail.com>
2021-11-14 13:17:18 -0500

    added notifications and settings

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,7 +1,11 @@
 const foods = require('./foods/foods.service.js');
 const items = require('./items/items.service.js');
+const notifications = require('./notifications/notifications.service.js');
+const settings = require('./settings/settings.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(foods);
   app.configure(items);
+  app.configure(notifications);
+  app.configure(settings);
 };
